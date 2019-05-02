b5347b58a3caa1715d95345fd9e66848b56fb71c
yang930729 <wapffhs@naver.com>
2019-05-02 18:39:16 +0900

    학원에서

--- DevTestGitHub/Assets/Scripts/TestHelloWorld.cs
+++ DevTestGitHub/Assets/Scripts/TestHelloWorld.cs
@@ -9,7 +9,7 @@ public class TestHelloWorld : MonoBehaviour
     {
         //Debug.Log("HelloWorld");
 
-        Debug.Log("안녕하세요 나연옆자리입니다!!학원.");
+        Debug.Log("안녕하세요 나연옆자리입니다!!학원2222222.");
     }
 
     // Update is called once per frame
